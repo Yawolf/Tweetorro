4aa6567bf07f27f2a21ce4fa5806a88cc3ee6048
Yago <yagowolf47@gmail.com>
2015-11-11 23:40:56 +0100

    First step of merging

--- src/server/Server.scala
+++ src/server/Server.scala
@@ -3,26 +3,32 @@ package server
 import java.rmi.server.UnicastRemoteObject
 import java.rmi.registry.Registry
 import java.rmi.registry.LocateRegistry
+import scala.language.postfixOps
 import com.redis._
 
 class Server extends ServerTrait {
   val db: RedisClient = new RedisClient("localhost",6379)
 
   def createUser(user: String, pass: String): Boolean = {
-    val db_user = "TWEETORRO:USERS:".concat(user).concat("PASSWORD")
-    if (db.exists(db_user))
+    val dbUser = s"TWEETORRO:USERS:$user"
+    if (db.exists(s"$dbUser:PASSWORD"))
       false
-    else
-      db.set(db_user,pass)
+    else {
+      db.set(s"$dbUser:PASSWORD",pass)
+      db.set(s"$dbUser:ONLINE", false)
+
+    }
   }
 
   def login(user: String, pass: String) : Boolean = {
-    val db_user = "TWEETORRO:USERS:".concat(user).concat("PASSWORD")
+    val db_user = s"TWEETORRO:USERS:$user:PASSWORD"
     if (db.exists(db_user)) {
       val user_pass = db.get(db_user)
       user_pass match {
         case Some(value) =>
           value == pass
+          val onLine= s"TWEETORRO:USERS:$user:ONLINE"
+          db.set(onLine,true)
         case None =>
           false
       }
@@ -36,7 +42,8 @@ object Server {
   def main(args: Array[String]): Unit = {
     
     try {
-      val server : ServerTrait = new Server
+      // val server: ServerTrait = new Server
+      val server: ServerTrait = new Server
       val stub = UnicastRemoteObject.exportObject(server,0).asInstanceOf[ServerTrait]
       val registry = LocateRegistry.createRegistry(1099)
       registry.rebind("tweetorro", stub)
