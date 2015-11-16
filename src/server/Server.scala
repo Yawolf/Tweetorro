d2ceb3b6d23cca6a21989e2e2db6a0437a91ce88
Yago <yagowolf47@gmail.com>
2015-11-17 00:41:14 +0100

    Just a little cheat to fix a failed merge request

--- src/server/Server.scala
+++ src/server/Server.scala
@@ -5,17 +5,67 @@ import java.rmi.registry.Registry
 import java.rmi.registry.LocateRegistry
 import scala.language.postfixOps
 import com.redis._
+import sun.util.calendar.JulianCalendar.Date
+import java.util.Calendar
 
 class Server extends ServerTrait {
   val db: RedisClient = new RedisClient("localhost",6379)
+  
+  def getTweets(user: String,number: Int): List[(String, String, String, String)] = {
+    val listaIDs = db.lrange(s"TWEETORRO:USERS:$user:TWEETS",0,-1)
+    listaIDs.get.flatten.map(getTweetTuple(_)) takeRight(number)
+  }
 
+  def getTweetTuple(tweet: String): (String, String, String, String) = {
+    (db.get(s"TWEETORRO:TWEETS:$tweet:USER").get,
+      db.get(s"TWEETORRO:TWEETS:$tweet:MESSAGE").get,
+      db.get(s"TWEETORRO:TWEETS:$tweet:DATE").get,
+      db.get(s"TWEETORRO:TWEETS:$tweet:ID").get)
+  }
+  
+  def sendTweet(tweet: (String, String, String)): Boolean = {
+    val id = db.incr("TWEETORRO:tweetID") getOrElse " "
+    val message = tweet._2
+    db.lpush(s"TWEETORRO:USERS:${tweet._1}:TWEETS", s"tweet$id")
+
+    val lista = db.lrange(s"TWEETORRO:USERS:$tweet._1:FOLLOWERS", 0,-1)
+    lista.get.flatten.map(x => db.lpush(s"TWEETORRO:USERS:$x:TWEETS", s"tweet$id"))
+
+    db.set(s"TWEETORRO:TWEETS:tweet$id:USER", tweet._1)
+    db.set(s"TWEETORRO:TWEETS:tweet$id:MESSAGE",message)
+    db.set(s"TWEETORRO:TWEETS:tweet$id:DATE",tweet._3)
+    db.set(s"TWEETORRO:TWEETS:tweet$id:ID",s"tweet$id")
+    true
+  }
+  
+  def retweet(user: String, tweetID: String): Boolean = {
+    val lista = db.lrange(s"TWEETORRO:USERS:$user:FOLLOWERS", 0,-1)
+    lista.get.flatten.foreach { x => db.lpush(s"TWEETORRO:USERS:$x:TWEETS", tweetID) }
+    db.lpush(s"TWEETORRO:USERS:$user:TWEETS", tweetID)
+    true
+  }
+  
+  def followers(user: String,number: Int): List[String] = {
+    val lista = db.lrange(s"TWEETORRO:USERS:$user:FOLLOWERS", 0,number)
+    lista.get.flatten
+  }
+  
+  def following(user: String,number: Int): List[String] = {
+    val lista = db.lrange(s"TWEETORRO:USERS:$user:FOLLOWING", 0,number)
+    lista.get.flatten
+  }
+  
+  def logoutRemote(user: String): Boolean = {
+    db.set(s"TWEETORRO:USERS:$user:LOGGED",false)
+    true
+  }
   def createUser(user: String, pass: String): Boolean = {
     val dbUser = s"TWEETORRO:USERS:$user"
     if (db.exists(s"$dbUser:PASSWORD"))
       false
     else {
       db.set(s"$dbUser:PASSWORD",pass)
-      db.set(s"$dbUser:ONLINE", false)
+      db.set(s"$dbUser:LOGGED", false)
     }
   }
 
@@ -25,33 +75,56 @@ class Server extends ServerTrait {
       val user_pass = db.get(dbUserPass)
       user_pass match {
         case Some(value) =>
-          value == pass
-          db.set(s"TWEETORRO:USERS:$user:LOGGED",true)
+          (value == pass) && db.set(s"TWEETORRO:USERS:$user:LOGGED",true)
         case None =>
           false
       }
     } else
       false
   }
-
-  def setProfile(nombre: String, alias: String) : Boolean = {
-    false
+  
+  def checkRemoteProfile(user: String,param: String): Option[String] = {
+    db.get(s"TWEETORRO:USERS:$user:LOGGED") match {
+      case Some("true") =>
+        db.get(s"TWEETORRO:USERS:$user:$param")
+      case _ => 
+        None
+    }
+  }
+  
+  def modifyRemoteProfile(user: String, param: String, value: String): Boolean = { //TODO
+    db.set(s"TWEETORRO:USERS:$user:$param", value) match {
+      case true => 
+        true
+      case false => 
+        println("Some error ocurred setting the value at the server. =( ")
+        println("This shouldnt never ever ever ever happen... So if you see this line you are allowed to scream and panic")
+        false
+    }    
   }
   
   def follow(user:String, userF: String): Boolean = {
-    val userToFollow = s"TWEETORRO:USERS:$userF"
-    if (db.exists(s"$user:PASSWORD")) {
-      db.lpush(s"TWEETORRO:USERS:$user:FOLLOWING", userF)
-      db.lpush(s"TWEETORRO:USERS:$userF:FOLLOWED", user)
-      println("Added following y follower :D")
-      true
-    } else{
-      false
+    db.get(s"TWEETORRO:USERS:$user:LOGGED") match {
+      case Some("true") =>
+        db.lpush(s"TWEETORRO:USERS:$user:FOLLOWING", userF)
+        db.lpush(s"TWEETORRO:USERS:$userF:FOLLOWERS", user)
+        println("Added following y follower :D")
+        true
+      case _ => 
+        false
     }
   }
 
   def unfollow(userLogged: String, userF: String): Boolean = {
-    true
+    db.get(s"TWEETORRO:USERS:$userLogged:LOGGED") match {
+      case Some("true") =>
+        db.lrem(s"TWEETORRO:USERS:$userLogged:FOLLOWING", 1, userF)
+        db.lrem(s"TWEETORRO:USERS:$userF:FOLLOWERS", 1, userLogged)
+        true
+      case _ =>
+        println("Usuario no está loggeado")
+        false
+    }
   }
 
 }
@@ -66,6 +139,7 @@ object Server {
       registry.rebind("tweetorro", stub)
       
       println("Server ready :D")
+      println("")
     }
     catch {
       case e: Exception => e printStackTrace
