c815820fa720360ad68df1492a03ff668acb4a78
Cazaril <eugenio90@gmail.com>
2015-11-20 16:53:22 +0100

    added ClientTrait, a trait for the client callback

---
+++ src/server/ClientTrait.scala
@@ -0,0 +1,13 @@
+package server
+
+import java.rmi.Remote
+import java.rmi.RemoteException
+
+trait ClientTrait extends Remote { 
+  
+  @throws(classOf[RemoteException])
+  def notifyMe(): Unit
+  
+  @throws(classOf[RemoteException])
+  def getUserName():String
+}
